2c35c8d5dc9134c8edaa479a950ae5cfffb13696
arevaloyulian23-gif <arevalo.yulian23@gmail.com>
2026-03-08 18:22:15 -0500

    Agregando script.js al repositorio

---
+++ 3407187_YulianArevalo_WEEK_2/starter/script.js
@@ -0,0 +1,12 @@
+// la informacion se este trabajo esta basada en el sistema de la flota de buses de bogotaDC "Transmilenio"  
+const DOMAIN_NAME = "SISTEMA DE GESTION DE FLOTAS";
+const itemName = "Gestion Vehicular";
+const itemCategory = "informacion del vehiculo";
+const itemQuantityVehicle = 150;
+const itemPrice = 3550;
+const itemInOperation = true;
+const assignedDriver = null;
+console.log("===========================");
+console.log(`FICHA DE DATOS: ${DOMAIN_NAME}`);
+console.log("===========================");
+console.log("");
